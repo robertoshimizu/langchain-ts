b9737060c772af3abd67783516d8760f40027222
Roberto Shimizu <rshimizubr@gmail.com>
2024-03-05 19:48:09 -0300

    lcel example

--- src/anthropic.ts
+++ src/anthropic.ts
@@ -1,14 +1,25 @@
 import { ChatAnthropic } from '@langchain/anthropic'
+import { StringOutputParser } from '@langchain/core/output_parsers'
+import { ChatPromptTemplate } from '@langchain/core/prompts'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 async function main () {
+  const prompt = ChatPromptTemplate.fromMessages([
+    ['system', 'You are a world class technical documentation writer.'],
+    ['user', '{input}']
+  ])
+  const outputParser = new StringOutputParser()
   const chatModel = new ChatAnthropic({
     anthropicApiKey: process.env.ANTHROPIC_API_KEY
   })
 
-  const answer = await chatModel.invoke('what is LangSmith?')
+  const llmChain = prompt.pipe(chatModel).pipe(outputParser)
+
+  const answer = await llmChain.invoke({
+    input: 'what is quantum physics?'
+  })
 
   console.log(answer)
 }
