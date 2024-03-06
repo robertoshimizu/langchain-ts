3c05a408d57cd424f57de08547d1932ad0e7ad56
Roberto Shimizu <rshimizubr@gmail.com>
2024-03-06 08:08:07 -0300

    leave it just as chatModel

--- src/models/anthropic.ts
+++ src/models/anthropic.ts
@@ -1,6 +1,6 @@
 import { ChatAnthropic } from '@langchain/anthropic'
-import { StringOutputParser } from '@langchain/core/output_parsers'
-import { ChatPromptTemplate } from '@langchain/core/prompts'
+// import { StringOutputParser } from '@langchain/core/output_parsers'
+// import { ChatPromptTemplate } from '@langchain/core/prompts'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -9,20 +9,20 @@ export const chatModel = new ChatAnthropic({
   anthropicApiKey: process.env.ANTHROPIC_API_KEY
 })
 
-async function main () {
-  const prompt = ChatPromptTemplate.fromMessages([
-    ['system', 'You are a world class technical documentation writer.'],
-    ['user', '{input}']
-  ])
-  const outputParser = new StringOutputParser()
+// async function main () {
+//   const prompt = ChatPromptTemplate.fromMessages([
+//     ['system', 'You are a world class technical documentation writer.'],
+//     ['user', '{input}']
+//   ])
+//   const outputParser = new StringOutputParser()
 
-  const llmChain = prompt.pipe(chatModel).pipe(outputParser)
+//   const llmChain = prompt.pipe(chatModel).pipe(outputParser)
 
-  const answer = await llmChain.invoke({
-    input: 'what is quantum physics?'
-  })
+//   const answer = await llmChain.invoke({
+//     input: 'what is quantum physics?'
+//   })
 
-  console.log(answer)
-}
-// npx ts-node src/anthropic.ts
-void main()
+//   // console.log(answer)
+// }
+// // npx ts-node src/anthropic.ts
+// void main()
