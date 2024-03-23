bff47d624a8b6b336eb4995e18ce447d83051773
Roberto Shimizu <rshimizubr@gmail.com>
2024-03-23 17:23:43 -0300

    implemented DUT vec

--- src/intelliModel/intelli_vector.ts
+++ src/intelliModel/intelli_vector.ts
@@ -110,6 +110,145 @@ const get_tuss_info = new DynamicStructuredTool({
   }
 })
 
+class DUT {
+  private vectorStore!: PineconeStore
+  private selfQueryRetriever!: SelfQueryRetriever<any>
+  private initializationPromise: Promise<void>
+
+  constructor() {
+    this.initializationPromise = this.init()
+  }
+
+  async init() {
+    try {
+      const pc = new Pinecone({
+        apiKey: process.env.PINECONE_API_KEY!
+      })
+      const indexes = await await pc.listIndexes()
+      //console.log('Indexes:', indexes)
+      const dut_index_info = await pc.describeIndex('dut')
+      console.log('dut index: ', dut_index_info)
+      const pineconeIndex = pc.Index('dut')
+
+      this.vectorStore = await PineconeStore.fromExistingIndex(
+        new OpenAIEmbeddings(),
+        { pineconeIndex }
+      )
+
+      const attributeInfo: AttributeInfo[] = [
+        {
+          name: 'numero',
+          description:
+            'numero da DUT - Diretriz de Utilizacao para Cobertura de Procedimento',
+          type: 'string'
+        },
+        {
+          name: 'nome',
+          description: 'Nome da DUT',
+          type: 'string'
+        }
+      ]
+      const llm = new ChatOpenAI()
+      const documentContents =
+        'Diretrizes de Utilizacao (DUT) para Cobertura de Procedimentos na Saude Suplementar publicados no anexo 2 da Resolucao 465/2021 da ANS'
+
+      this.selfQueryRetriever = await SelfQueryRetriever.fromLLM({
+        llm,
+        vectorStore: this.vectorStore,
+        documentContents,
+        attributeInfo,
+        structuredQueryTranslator: new FunctionalTranslator()
+      })
+
+      console.log('PineconeStore initiated ...')
+    } catch (error) {
+      console.error('Opss, error initiating PineconeStore', error)
+    }
+  }
+
+  public async ensureInitialized() {
+    await this.initializationPromise
+  }
+
+  async search_by_term(query: string) {
+    await this.ensureInitialized() // Ensure the class is initialized before searching
+    try {
+      const results = await this.vectorStore.similaritySearchWithScore(query)
+      console.log('Results:', results)
+      return JSON.stringify(results)
+    } catch (error) {
+      console.error('Error searching PineconeStore', error)
+    }
+  }
+  async search_by_number(dut_number: string) {
+    await this.ensureInitialized() // Ensure the class is initialized before searching
+    try {
+      // const results = await this.selfQueryRetriever.getRelevantDocuments(
+      //   `return the DUT for the number:  ${dut_number.toString()}`
+      // )
+      this.vectorStore.filter = {
+        numero: { $eq: dut_by_number.toString() }
+      }
+      const results = await this.vectorStore.similaritySearch(
+        `Find the DUT for the number: ${dut_number.toString()}`
+      )
+      console.log('Results:', results)
+      return JSON.stringify(results)
+    } catch (error) {
+      console.error('Error searching PineconeStore', error)
+    }
+  }
+}
+const dut_by_term = async (query: string) => {
+  console.log('dut_by_term')
+  const dut = new DUT()
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  const res = await dut.search_by_term(query)
+  return res?.toString()
+}
+
+const dut_by_number = async (query: string) => {
+  console.log('dut_by_term')
+  const dut = new DUT()
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  const res = await dut.search_by_number(query)
+  return res?.toString()
+}
+
+const get_dut_by_term = new DynamicStructuredTool({
+  name: 'DUT_SearchByTerm',
+  description: `Call this to get the DUT of a medical, therapeutic or diagnostic procedure. Diretriz de Utilização para Cobertura 
+                de Procedimento na Saúde Suplementar, comumente referida como DUT (Diretriz de Utilização de Tecnologia em Saúde)`,
+  schema: z.object({
+    terms_of_dut: z
+      .string()
+      .describe(
+        'terms that describes the DUT, which can be a medical, therapeutic or diagnostic procedure'
+      )
+  }),
+  func: async ({ terms_of_dut }) => {
+    const result = await dut_by_term(terms_of_dut)
+    return result || '' // Ensure a string value is always returned, even if it is undefined
+  }
+})
+
+const get_dut_by_number = new DynamicStructuredTool({
+  name: 'DUT_SearchByNumber',
+  description: `Call this to get the DUT of a medical, therapeutic or diagnostic procedure. Diretriz de Utilização para Cobertura 
+                de Procedimento na Saúde Suplementar, comumente referida como DUT (Diretriz de Utilização de Tecnologia em Saúde)`,
+  schema: z.object({
+    dut_number: z
+      .string()
+      .describe(
+        'number that refers to the index of a DUT in the annex II of the RN 465/2021 of ANS'
+      )
+  }),
+  func: async ({ dut_number }) => {
+    const result = await dut_by_number(dut_number)
+    return result || '' // Ensure a string value is always returned, even if it is undefined
+  }
+})
+
 class Agent {
   private llm: any
   private tools: DynamicTool[]
@@ -126,11 +265,11 @@ class Agent {
     this.prompt = ChatPromptTemplate.fromMessages([
       [
         'system',
-        `You are a helpful assistant to provide the TUSS data for a medical term. You are going to research and provide the information to another llm model downstream.
+        `You are a helpful assistant to provide data for a medical term. You are going to research and provide the information to another llm model downstream.
         There is a close relationship among TUSS, the ANS list (rol ANS), and the Technical Use Guidelines (DUTs). If during the search, it retrieves more than one document,
         then you MUST provide all the documents for all of them. YOU must provide the llm downstream the most complete information so it can have conditions to answer its questions.
         YOU MUST answer in the language idiom of the query.
-        If the term does not relate to a TUSS, or if you cannot find any information you MUST return an empty screen.`
+        If you cannot find any information you MUST return an empty screen.`
       ],
       ['user', '{input}'],
       new MessagesPlaceholder('agent_scratchpad')
@@ -161,7 +300,7 @@ class Agent {
 }
 
 export function getAgent() {
-  const tools = [get_tuss_info]
+  const tools = [get_tuss_info, get_dut_by_term, get_dut_by_number]
   const agent = new Agent(tools)
   return agent
 }
